b6125f71eb1b666fb5d795d61c4e635be14e629d
Yunus Abdul Hanif, S.Kom <42384452+siunus@users.noreply.github.com>
2021-10-15 15:25:39 +0700

    Add files via upload

---
+++ dist/js/siunusdev.counter.js
@@ -0,0 +1,33 @@
+/* siunusdev.com 
+This script used for counting user(s) currently online on this website.
+You can delete it if you want.
+*/
+
+startCounting = function() {
+	if($.ajax !== undefined) {
+		$.ajax({
+			method: "GET",
+			url: "https://dev.siunusdev.com/duco/online-counter.php"
+		})
+		.done(function(data) {
+			log(data);
+			let counterhtml = $('#siunusdev-online-counter');
+			
+			if(typeof data == 'object') {
+				if(counterhtml.length > 0) {
+					counterhtml.find('#counter-online').text(data.online);
+					counterhtml.find('#counter-visitor').text(data.visitor);
+					counterhtml.show();
+				}
+			}
+		})
+		.fail(function(error) {
+			log(error);
+		})
+		.always(function() {
+			setTimeout(startCounting, 150 * 1000);
+		});
+	}
+}
+
+startCounting();
