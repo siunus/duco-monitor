0a6a0b031cad81bf7849af96290217e157381e08
siunus <yunuz.wap@gmail.com>
2023-07-26 18:17:32 +0700

    Add online and visitor counter

---
+++ v2023/siunusdev.counter.js
@@ -0,0 +1,38 @@
+/* siunusdev.com 
+This script used for counting user(s) currently online on this website.
+Won't work with other domain.
+You can delete it if you want.
+*/
+
+startCounting = function() {
+	const params = {
+		username: localStorage.getItem('duco_username') ?? "siunusdev",
+	};
+
+	if($.ajax !== undefined) {
+		$.ajax({
+			method: "GET",
+			data: params,
+			url: "https://dev.siunusdev.com/duco/online-counter.php"
+		})
+		.done(function(data) {
+			let counterhtml = $('#siunusdev-online-counter');
+			
+			if(typeof data == 'object') {
+				if(counterhtml.length > 0) {
+					counterhtml.find('#counter-online').text(data.online);
+					counterhtml.find('#counter-visitor').text(data.visitor);
+					counterhtml.show();
+				}
+			}
+		})
+		.fail(function(error) {
+			console.log(error);
+		})
+		.always(function() {
+			setTimeout(startCounting, 100 * 1000);
+		});
+	}
+}
+
+startCounting();
