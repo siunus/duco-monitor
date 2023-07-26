ed8171353fd28564c0623c483c18b0a1232c49dc
siunus <yunuz.wap@gmail.com>
2023-07-26 10:17:21 +0700

    Update colors config

--- v2023/assets/js/config.js
+++ v2023/assets/js/config.js
@@ -5,23 +5,61 @@
  * ! To clear local storage: (https://www.leadshook.com/help/how-to-clear-local-storage-in-google-chrome-browser/).
  */
 
-'use strict';
+"use strict";
 
 // JS global variables
+// let config = {
+//   colors: {
+//     primary: '#696cff',
+//     secondary: '#8592a3',
+//     success: '#71dd37',
+//     info: '#03c3ec',
+//     warning: '#ffab00',
+//     danger: '#ff3e1d',
+//     dark: '#233446',
+//     black: '#000',
+//     white: '#fff',
+//     body: '#f4f5fb',
+//     headingColor: '#566a7f',
+//     axisColor: '#a1acb8',
+//     borderColor: '#eceef1'
+//   }
+// };
+
 let config = {
   colors: {
-    primary: '#696cff',
-    secondary: '#8592a3',
-    success: '#71dd37',
-    info: '#03c3ec',
-    warning: '#ffab00',
-    danger: '#ff3e1d',
-    dark: '#233446',
-    black: '#000',
-    white: '#fff',
-    body: '#f4f5fb',
-    headingColor: '#566a7f',
-    axisColor: '#a1acb8',
-    borderColor: '#eceef1'
-  }
+    primary: "#696cff",
+    secondary: "#8592a3",
+    success: "#71dd37",
+    info: "#03c3ec",
+    warning: "#ffab00",
+    danger: "#ff3e1d",
+    dark: "#233446",
+    black: "#000",
+    white: "#fff",
+    cardColor: "#fff",
+    bodyBg: "#f5f5f9",
+    bodyColor: "#697a8d",
+    headingColor: "#566a7f",
+    textMuted: "#a1acb8",
+    borderColor: "#eceef1",
+  },
+  colors_label: {
+    primary: "#666ee81a",
+    secondary: "#8897aa1a",
+    success: "#28d0941a",
+    info: "#1e9ff21a",
+    warning: "#ff91491a",
+    danger: "#ff49611a",
+    dark: "#181c211a",
+  },
+  colors_dark: {
+    cardColor: "#2b2c40",
+    bodyBg: "#232333",
+    bodyColor: "#a3a4cc",
+    headingColor: "#cbcbe2",
+    textMuted: "#7071a4",
+    borderColor: "#444564",
+  },
+  enableMenuLocalStorage: true,
 };
